cc719de5a3758a028f3eb5dab58284f4e7c9a025
damaggu <markusm2342@gmail.com>
2024-04-15 16:50:51 -0700

    adding dependencies

--- setup_mm.sh
+++ setup_mm.sh
@@ -1,6 +1,8 @@
 conda init bash && source ~/.bashrc &&
-conda env create -f VPD/stable-diffusion/environment.yaml &&
+conda env create -f stable-diffusion/environment.yaml &&
 conda activate ldm
 
 pip install openmim &&
 mim install mmcv-full==1.6.2 mmsegmentation==0.30.0
+pip install timm wandb
+pip install pytorch_lightning==1.6.5
