6d43a1b593023019d054c6796d5b330385adb551
Neehar Kondapaneni <nkondapa@caltech.edu>
2024-04-17 10:43:45 -0700

    updated environmen yaml (testing)

--- setup_mm.sh
+++ setup_mm.sh
@@ -1,8 +1,8 @@
 conda init bash && source ~/.bashrc &&
 conda env create -f environment.yaml &&
-conda activate ldm
+conda activate tadp
 
+#pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
 pip install openmim &&
 mim install mmcv-full==1.6.2 mmsegmentation==0.30.0
-pip install timm wandb
-pip install pytorch_lightning==1.6.5
+#pip install pytorch_lightning==1.6.5
