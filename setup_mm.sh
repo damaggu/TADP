28c3c8288036b73a7a82924d29a5bbe2c10d77c9
Neehar Kondapaneni <nkondapa@caltech.edu>
2024-04-17 11:50:18 -0700

    load lightning and pytorch lightning

--- setup_mm.sh
+++ setup_mm.sh
@@ -2,7 +2,9 @@ conda init bash && source ~/.bashrc &&
 conda env create -f environment.yaml &&
 conda activate tadp
 
-#pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
 pip install openmim &&
 mim install mmcv-full==1.6.2 mmsegmentation==0.30.0
-#pip install pytorch_lightning==1.6.5
+
+git clone https://github.com/damaggu/stable-diffusion
+mv stable-diffusion stable_diffusion
+cp -r stable_diffusion/ldm/ ./
