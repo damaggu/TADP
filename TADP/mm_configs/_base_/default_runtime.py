ecf9d1c7acc119e72b15dcea8ddd7cf5e2724e4d
damaggu <markusm2342@gmail.com>
2024-04-17 17:16:26 -0700

    replace wandb naming

--- TADP/mm_configs/_base_/default_runtime.py
+++ TADP/mm_configs/_base_/default_runtime.py
@@ -5,8 +5,8 @@ log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook', by_epoch=False),
-        dict(type='MMSegWandbHook', by_epoch=False, init_kwargs=dict(project='madman',
-                                                                      entity='vision-lab',
+        dict(type='MMSegWandbHook', by_epoch=False, init_kwargs=dict(project=os.environ.get("WANDB_PROJECT") or 'tadp_seg',
+                                                                      entity=os.environ.get("WANDB_ENTITY") or 'default',
                                                                       group=os.environ.get("GROUP_NAME") or f'vpd_mmseg',
                                                                       name=os.environ.get("RUN_NAME") or f'vpd_ade20k'
                                                                       ),
