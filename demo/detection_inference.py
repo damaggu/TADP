fafe4f1e5703d4faea486bba66fa4018b154e6b4
damaggu <markusm2342@gmail.com>
2024-04-17 17:45:16 -0700

    cleanup it 1

--- demo/detection_inference.py
+++ demo/detection_inference.py
@@ -2,32 +2,30 @@ import lightning.pytorch as pl
 import torch
 import yaml
 from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 from TADP.tadp_objdet import TADPObj
+from TADP.utils.detection_utils import voc_classes
 
 pl.seed_everything(42)
 # load model from checkpoint
 
 cfg = yaml.load(open("./sd_tune.yaml", "r"), Loader=yaml.FullLoader)
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
 cfg["freeze_text_adapter"] = True
-# cfg['text_conditioning'] = "class_emb"
 cfg['blip_caption_path'] = 'captions/voc_extended_train_val_captions.json'
 cfg['cross_blip_caption_path'] = 'captions/watercolor_captions.json'
 cfg["append_self_attention"] = False
-# If using textual-inversion, uncomment #
 cfg['text_conditioning'] = "blip"
 cfg['textual_inversion_token_path'] = "model_personalization/tokens/water_color_50/"
-# cfg['textual_inversion_token_path'] = None
-
 cfg['textual_inversion_caption_path'] = "textual_inversion_captions/voc_extended_captions.json"
 cfg['dreambooth_checkpoint'] = None
 cfg['val_dataset_name'] = "watercolor"
 cfg['use_scaled_encode'] = True
 
 load_checkpoint_path = '/mnt/m4_data/watercolor_checkpoints/checkpoint_epoch_2.pt'
-
-from TADP.utils.detection_utils import voc_classes
 
 detection_model = TADPObj(class_embedding_path="./data/pascal_class_embeddings.pth", cfg=cfg, class_names=voc_classes)
 detection_model.to(device)
@@ -52,11 +50,6 @@ scores = scores[scores > threshold]
 # resize image to 512x512
 img = img.resize((512, 512))
 
-# overlay boxes on image
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-
 fig, ax = plt.subplots(1)
 ax.imshow(img)
 for box, label, score in zip(boxes, labels, scores):
@@ -67,4 +60,3 @@ for box, label, score in zip(boxes, labels, scores):
 plt.show()
 
 print(a)
-
